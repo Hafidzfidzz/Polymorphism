6007fbc66b17d3a448a841332db6a84122f6b65e
Hafidzfidzz <48426232+Hafidzfidzz@users.noreply.github.com>
2019-05-14 14:54:02 +0700

    Add files via upload

---
+++ LatihanPolymorphism/LatihanPolymorphism/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Speak();
+
+            Cat cat = new Cat();
+            animal = cat; 
+            //cat.Speak();
+            animal.Speak();
+
+            Chicken chicken = new Chicken();
+            animal = cat;
+            //chicken.Speak();
+            animal.Speak();
+
+            Dog dog = new Dog();
+            animal = cat;
+            //dog.Speak();
+            animal.Speak();
+
+            Console.ReadKey();
+        }
+    }
+}
